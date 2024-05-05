04422c75f96520731ee8effd6328aa212e8d1275
Engraya <ahmadyakubuaya@gmail.com>
2024-05-05 14:19:11 +0100

    Project and Files Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@components/Header";
+import ActiveSectionContextProvider from "@context/ActiceSectionContext";
+import Footer from "@components/Footer";
+import ThemeSwitch from "@components/ThemeSwitch";
+import ThemeContextProvider from "@context/ThemeContext";
+import { Toaster } from "react-hot-toast"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +27,16 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-          {children}
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header/>
+            {children}
+            <Footer/>
+            <Toaster position="top-right" />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
+      
         </body>
     </html>
   );
