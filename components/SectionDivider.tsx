45233877389067788f7ed87fb690fc58b9abb053
Engraya <ahmadyakubuaya@gmail.com>
2024-05-05 14:31:18 +0100

    Files Config

---
+++ components/SectionDivider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SectionDivider() {
+  return (
+    <div>
+      <h1>Section Divider</h1>
+    </div>
+  )
+}
+
+export default SectionDivider
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import About from "@components/About";
+import Introduction from "@components/Introduction";
+import Experience from "@components/Experience";
+import Projects from "@components/Projects";
+import Skills from "@components/Skills";
+import Contact from "@components/Contact";
+import SectionDivider from "@components/SectionDivider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Engr Ahmad</h1>
+    <main className="flex flex-col items-center px-4">
+    <Introduction />
+    <SectionDivider />
+    <About />
+    <Projects />
+    <Skills />
+    <Experience />
+    <Contact />
     </main>
   );
 }
